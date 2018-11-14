abb5b4a59006f047f3ad7f7cf3e8c327aaebc698
Salih Kardan <salih@opsgenie.com>
2018-11-14 20:25:49 +0300

    sample routing

--- public/components/main/app.js
+++ public/components/main/app.js
@@ -43,7 +43,7 @@ class App extends Component {
 
     render() {
         return (
-            <div className="overview">
+            <div>
                 <div>
                     <Nav tabs>
                         <NavItem>
