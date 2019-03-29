4ed4464c8444db0f496cede9de08963e378f35b2
milkers <ilker1486@gmail.com>
2019-03-29 09:21:23 +0300

    overview graphs improved with recharts

--- public/containers/OverviewGraphsContainer/OverviewGraphsContainer.js
+++ public/containers/OverviewGraphsContainer/OverviewGraphsContainer.js
@@ -26,7 +26,20 @@ import {
     fetchInvocationDurationsPerHourByName
 } from "../../store/actions";
 
+import {
+    BarChart,
+    Bar,
+    LineChart,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Tooltip,
+    Legend,
+    Line
+} from 'recharts';
+
 import { connect } from "react-redux";
+import moment from "moment";
 
 const { SCALE, ORIENTATION } = EuiSeriesChartUtils;
 const { CURVE_MONOTONE_X } = EuiSeriesChartUtils.CURVE;
@@ -68,21 +81,21 @@ class OverviewGraphsContainer extends React.Component {
         let arr = this.props.invocationsByFunctions;
         for (let i = arr.length - 1; i >= 0; i--) {
             let obj = arr[i];
-            DATA.push({ x: obj.doc_count, y: obj.key });
+            DATA.push({ invocationCount: obj.doc_count, y: obj.key });
         }
 
         const EDATA = [];
         let eArr = this.props.errInvocationsByFunctions;
         for (let i = eArr.length - 1; i >= 0; i--) {
             let obj = eArr[i];
-            EDATA.push({ x: obj.doc_count, y: obj.key });
+            EDATA.push({ invocationCount: obj.doc_count, y: obj.key });
         }
 
         const CDATA = [];
         let cArr = this.props.csInvocationsByFunctions;
         for (let i = cArr.length - 1; i >= 0; i--) {
             let obj = cArr[i];
-            CDATA.push({ x: obj.doc_count, y: obj.key });
+            CDATA.push({ invocationCount: obj.doc_count, y: obj.key });
         }
 
         return (
@@ -91,39 +104,57 @@ class OverviewGraphsContainer extends React.Component {
                     <EuiText grow={false}>
                         <p>Top 10 invoked functions</p>
                     </EuiText>
-                    <EuiSeriesChart
-                        yType={SCALE.ORDINAL}
-                        orientation={ORIENTATION.HORIZONTAL}
+                    <BarChart
+                        onClick={(e) => this.onClick(e)}
+                        data={DATA}
+                        width={300}
                         height={250}
-                    >
-                        <EuiBarSeries name="Invocation Count" data={DATA} color={"#65a637"} onValueClick={(e) => this.onClick(e)} />
-                    </EuiSeriesChart>
+                        layout="vertical">
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis type="number" />
+                        <YAxis dataKey="y" type="category" />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="invocationCount" fill="#37DC94" />
+                    </BarChart>
                 </EuiFlexItem>
 
                 <EuiFlexItem>
                     <EuiText grow={false}>
                         <p>Top 10 erroneous functions</p>
                     </EuiText>
-                    <EuiSeriesChart
-                        yType={SCALE.ORDINAL}
-                        orientation={ORIENTATION.HORIZONTAL}
+                    <BarChart
+                        onClick={(e) => this.onClick(e)}
+                        data={EDATA}
+                        width={300}
                         height={250}
-                    >
-                        <EuiBarSeries name="Invocation Count" data={EDATA} color={"#d93f3c"} onValueClick={(e) => this.onClick(e)} />
-                    </EuiSeriesChart>
+                        layout="vertical">
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis type="number" />
+                        <YAxis dataKey="y" type="category" />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="invocationCount" fill="#FF5126" />
+                    </BarChart>
                 </EuiFlexItem>
 
                 <EuiFlexItem>
                     <EuiText grow={false}>
                         <p>Top 10 cold started functions</p>
                     </EuiText>
-                    <EuiSeriesChart
-                        yType={SCALE.ORDINAL}
-                        orientation={ORIENTATION.HORIZONTAL}
+                    <BarChart
+                        onClick={(e) => this.onClick(e)}
+                        data={CDATA}
+                        width={300}
                         height={250}
-                    >
-                        <EuiBarSeries name="Invocation Count" data={CDATA} color={"#6db7c6"} onValueClick={(e) => this.onClick(e)} />
-                    </EuiSeriesChart>
+                        layout="vertical">
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis type="number" />
+                        <YAxis dataKey="y" type="category" />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="invocationCount" fill="#162C9B" />
+                    </BarChart>
                 </EuiFlexItem>
 
             </EuiFlexGroup>
@@ -132,13 +163,16 @@ class OverviewGraphsContainer extends React.Component {
     }
 
     onClick = (e) => {
-        const {startDate, interval} = this.props;
-        const functionName = e.y;
+        const { startDate, interval } = this.props;
+        // const functionName = e.y;
+        if (e && e.activeLabel) {
+            const functionName = e.activeLabel;
 
-        this.props.fetchInvocationCountsPerHourByName(this.props.httpClient, startDate, interval, functionName);
-        this.props.fetchInvocationDurationsPerHourByName(this.props.httpClient, startDate, interval, functionName);
+            this.props.fetchInvocationCountsPerHourByName(this.props.httpClient, startDate, interval, functionName);
+            this.props.fetchInvocationDurationsPerHourByName(this.props.httpClient, startDate, interval, functionName);
 
-        this.setState({selectedFunctionName: functionName});
+            this.setState({ selectedFunctionName: functionName });
+        }
     };
 
 
@@ -147,7 +181,8 @@ class OverviewGraphsContainer extends React.Component {
         const DATA_A = [];
         for (let key in this.props.invocationCountsPerHour) {
             let obj = this.props.invocationCountsPerHour[key];
-            DATA_A.push({ x: obj.key, y: obj.doc_count });
+            // DATA_A.push({ x: obj.key, y: obj.doc_count });
+            DATA_A.push({ x: moment(obj.key).format("HH:mm"), count: obj.doc_count });
         }
 
         myData[0] = {
@@ -159,7 +194,8 @@ class OverviewGraphsContainer extends React.Component {
         const DATA_B = [];
         for (let key in this.props.invocationDurationsPerHour) {
             let obj = this.props.invocationDurationsPerHour[key];
-            DATA_B.push({ x: obj.key, y: Number((obj.duration.value / 60000).toFixed(2)) });
+            // DATA_B.push({ x: obj.key, y: Number((obj.duration.value / 60000).toFixed(2)) });
+            DATA_B.push({ x: moment(obj.key).format("HH:mm"), duration: Number((obj.duration.value / 60000).toFixed(2)) });
         }
 
         yourData[0] = {
@@ -174,22 +210,42 @@ class OverviewGraphsContainer extends React.Component {
                         <EuiText grow={false}>
                             <p> Total invocation count for <EuiTextColor color="subdued"> {this.state.selectedFunctionName == null ? 'all' : this.state.selectedFunctionName}</EuiTextColor> function(s)</p>
                         </EuiText>
-                        <EuiSeriesChart height={250} xType={SCALE.TIME}>
-                            {myData.map((d, i) => (
-                                <EuiLineSeries key={`count-${i}`} name={d.name} data={d.data} showLineMarks={false} curve={CURVE_MONOTONE_X} />
-                            ))}
-                        </EuiSeriesChart>
+                        <LineChart
+                            data={DATA_A}
+                            syncId="metricChart"
+                            width={450}
+                            height={250}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="x" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+
+                            <Line type="monotone" dataKey="count" stroke="#37DC94" />
+
+                        </LineChart>
                     </EuiFlexItem>
 
                     <EuiFlexItem>
                         <EuiText grow={false}>
                             <p>Total  Invocation duration for <EuiTextColor color="subdued"> {this.state.selectedFunctionName == null ? 'all' : this.state.selectedFunctionName}</EuiTextColor> function(s)</p>
                         </EuiText>
-                        <EuiSeriesChart height={250} xType={SCALE.TIME}>
-                            {yourData.map((d, i) => (
-                                <EuiLineSeries key={`duration-${i}`} name={d.name} data={d.data} showLineMarks={false} curve={CURVE_MONOTONE_X} />
-                            ))}
-                        </EuiSeriesChart>
+                        <LineChart
+                            data={DATA_B}
+                            syncId="metricChart"
+                            width={450}
+                            height={250}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="x" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+
+                            <Line type="monotone" dataKey="duration" stroke="#37DC94" />
+
+                        </LineChart>
                     </EuiFlexItem>
 
                 </EuiFlexGrid>
